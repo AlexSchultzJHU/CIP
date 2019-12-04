cd9e83843f9d14280fc32438c83dd0f9e9c582bf
AlexSchultzJHU <aschul19@jhu.edu>
2019-12-04 01:57:09 -0500

    Working Random Pattern, next step is to follow higher phermores

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 
 import time
+import random
 import socket
 import netifaces as ni
 
@@ -12,7 +13,7 @@ import threading
 decSlice = None
 mySlice = None
 
-arraySize = 12
+arraySize = 23
 
 
 def refreshFormicariumSlice():
@@ -31,15 +32,17 @@ def displayFormicariumSlice():
 
     global mySlice
     
+    displayTime = 120
     i = 0
-    while ( i < 10 ):
+    while ( i < displayTime ):
         refreshFormicariumSlice()
         time.sleep(1)
         i = i + 1
         print("\nExamining at second: " + str( i) )
         print( mySlice )
         print("\n")
-    return
+    return True
+
 #    ctr = 0
 #    for i in mySlice:
 #        try:
@@ -64,12 +67,56 @@ def createFormicariumSlice(a,b):
 
     mySlice[a][b] = 10
 
+def updateeFormicariumSlice(a,b, value = 10 ):
+    global mySlice
+    mySlice[a][b] = value + mySlice[a][b]
+    return True
 
-def createForagingAnt( a,b, antLife = 255 ):
+def randomDirection( ):
+    x = random.randint(-1,1)
+    y = random.randint(-1,1)
+    #print(str( x) + " " + str(y) )
+    return x,y
+
+def boundaryCheck( gridInput ):
+    global arraySize
+    if gridInput <= 0:
+        return 0
+    elif gridInput >= arraySize:
+        return arraySize - 1
+    else:
+        return gridInput
+
+
+#PortScan
+def checkLocation( a,b ):
+    if random.randint( 0, 12) + a + b % 7== 3:
+        return True
+    else:
+        return False
+
+def antForagesRandom( a,b, antLife = 255 ):
     #Start = a,b
     #Anyt Life is 
+    x,y = randomDirection()
+    a = boundaryCheck( a + x )
+    b = boundaryCheck( b + y )
+    if checkLocation( a,b ):
+        updateeFormicariumSlice(a,b, 10)
+    else:
+        updateeFormicariumSlice(a,b, 3 )
 
+    return a,b
 
+def createForagingAnt( a,b, antLife = 255 ):
+    currentA = a
+    currentB = b
+    while (antLife > 0):
+        time.sleep(1)
+        currentA,currentB = antForagesRandom( currentA,currentB )
+        antLife = antLife - 1
+
+    return True
 
 def main():
     ip = ni.ifaddresses('eth1')[2][0]['addr'].split(".")
@@ -81,19 +128,39 @@ def main():
     a=3
     b=3
 
-
-
-
     createFormicariumSlice(a,b)
-    
+   
     
     sliceThread = threading.Thread( target = displayFormicariumSlice, args = () )
+    antOneThread = threading.Thread( target = createForagingAnt, args = (a,b) )
 
     #antOne will start here
     sliceThread.start()
+    antOneThread.start()
 
     sliceThread.join()
+    antOneThread.join()
+
+    #Add nts as need
+    antTwoThread = threading.Thread( target = createForagingAnt, args = (a,b) )
+    antTwoThread.start()
+    antTwoThread.join()
+    
+    antThrThread = threading.Thread( target = createForagingAnt, args = (a,b) )
+    antThrThread.start()
+    antThrThread.join()
+ 
+    antForThread = threading.Thread( target = createForagingAnt, args = (a,b) )
+    antForThread.start()
+    antForThread.join()
+
+
+
+
+
+
+
+
 
 main()
-
 
