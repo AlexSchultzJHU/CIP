2970fa50fce665c3e8565856ecbc1bfb9afaba5f
AlexSchultzJHU <aschul19@jhu.edu>
2019-12-04 03:07:58 -0500

    Attepmt to add color

--- search.py
+++ search.py
@@ -5,7 +5,11 @@ import socket
 import netifaces as ni
 
 
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib.pyplot import imshow, show, colorbar
+
+#import matplotlib.pyplot as plt
 import numpy as np
 import threading 
 
@@ -38,9 +42,15 @@ def displayFormicariumSlice():
         refreshFormicariumSlice()
         time.sleep(1)
         i = i + 1
-        print("\nExamining at second: " + str( i) )
-        print( mySlice )
-        print("\n")
+        
+        imshow( mySlice )
+        colorbar()
+        show()
+
+        
+        #rint("\nExamining at second: " + str( i) )
+        #rint( mySlice )
+        #rint("\n")
     return True
 
 #    ctr = 0
@@ -67,7 +77,7 @@ def createFormicariumSlice(a,b):
 
     mySlice[a][b] = 10
 
-def updateeFormicariumSlice(a,b, value = 10 ):
+def updateFormicariumSlice(a,b, value = 10 ):
     global mySlice
     mySlice[a][b] = value + mySlice[a][b]
     return True
@@ -76,6 +86,12 @@ def randomDirection( ):
     x = random.randint(-1,1)
     y = random.randint(-1,1)
     #print(str( x) + " " + str(y) )
+
+    #Eliminate staying still
+    if x == 0 and y == 0:
+        x,y = randomDirection()
+
+
     return x,y
 
 def boundaryCheck( gridInput ):
@@ -86,6 +102,45 @@ def boundaryCheck( gridInput ):
         return arraySize - 1
     else:
         return gridInput
+
+
+#Logically, dont want to follow exact same path
+#Will want to randomly pick left or right?
+#Goal: Find highest neighboring value, go to lower value 'left' or 'right' of it, random if tie
+# should not go backwards thus values of 9 or 10 should be 'behind' ant
+def bestDirection( a,b ):
+    global mySlice
+    
+    x = 0
+    y = 0
+
+    #Will revisit logic
+    n = mySlice[ boundaryCheck(a+1) ][ boundaryCheck(b) ] 
+    e = mySlice[ boundaryCheck(a) ][ boundaryCheck(b+1) ] 
+    s= mySlice[ boundaryCheck(a-1) ][ boundaryCheck(b) ] 
+    w = mySlice[ boundaryCheck(a) ][ boundaryCheck(b-1) ] 
+
+    cardinal = [ n, e, s, w]
+    myDir = max( cardinal )
+
+    if n == myDir:
+        x = -1
+        y = randomDirection()[0]
+    elif e == myDir:
+        y = -1
+        x = randomDirection()[0]
+    elif s == myDir:
+        x = +1
+        y = randomDirection()[0]
+    elif w == myDir:
+        y = +1
+        x = randomDirection()[0]
+
+    if x == 0 and y == 0:
+        x,y = randomDirection()
+
+    return x,y
+
 
 
 #PortScan
@@ -102,13 +157,26 @@ def antForagesRandom( a,b, antLife = 255 ):
     a = boundaryCheck( a + x )
     b = boundaryCheck( b + y )
     if checkLocation( a,b ):
-        updateeFormicariumSlice(a,b, 10)
+        updateFormicariumSlice(a,b, 10)
     else:
-        updateeFormicariumSlice(a,b, 3 )
-
+        updateFormicariumSlice(a,b, 3 )
     return a,b
 
-def createForagingAnt( a,b, antLife = 255 ):
+
+def antForagesFollowPath( a,b, antLife = 255 ):
+    #Start = a,b
+    #Anyt Life is 
+    x,y = bestDirection(a,b)
+    a = boundaryCheck( a + x )
+    b = boundaryCheck( b + y )
+    if checkLocation( a,b ):
+        updateFormicariumSlice(a,b, 10)
+    else:
+        updateFormicariumSlice(a,b, 3 )
+    return a,b
+ 
+
+def createScoutingAnt( a,b, antLife = 255 ):
     currentA = a
     currentB = b
     while (antLife > 0):
@@ -117,6 +185,18 @@ def createForagingAnt( a,b, antLife = 255 ):
         antLife = antLife - 1
 
     return True
+
+
+def createForagingAnt( a,b, antLife = 255 ):
+    currentA = a
+    currentB = b
+    while (antLife > 0):
+        time.sleep(1)
+        currentA,currentB = antForagesFollowPath( currentA,currentB )
+        antLife = antLife - 1
+
+    return True
+
 
 def main():
     ip = ni.ifaddresses('eth1')[2][0]['addr'].split(".")
@@ -129,33 +209,46 @@ def main():
     b=3
 
     createFormicariumSlice(a,b)
-   
-    
-    sliceThread = threading.Thread( target = displayFormicariumSlice, args = () )
-    antOneThread = threading.Thread( target = createForagingAnt, args = (a,b) )
 
-    #antOne will start here
+    sliceThread = threading.Thread( target = displayFormicariumSlice, args = () )
     sliceThread.start()
-    antOneThread.start()
+
+
+
+
+    print("Slice created")
+    maxScoutingAnts = 3
+    antScoutThreadArray = []
+    print("Start ant scouts: " )
+    for i in range( 0, maxScoutingAnts ):
+        print(str(i) + " Started" )
+        antScoutThread = threading.Thread( target = createScoutingAnt, args = (a,b) )
+        antScoutThread.start()
+        antScoutThreadArray.append( antScoutThread )
+        print(str(i) + " Done" )
+   
+    maxForagingAnts = 6
+    antForageThreadArray = []
+    print("Start ant forages: " )
+    for i in range( 0, maxForagingAnts ):
+        print(str(i) + " Started" )
+        antForageThread = threading.Thread( target = createForagingAnt, args = (a,b) )
+        antForageThread.start()
+        antForageThreadArray.append( antForageThread )
+        print(str(i) + " Done" )
+   
+    for ant in antScoutThreadArray:
+            print("Joining scout ant " ) 
+            ant.join()
+
+    for ant in antForageThreadArray:
+            print("Joining forage ant " ) 
+            ant.join()
 
     sliceThread.join()
-    antOneThread.join()
 
-    #Add nts as need
-    antTwoThread = threading.Thread( target = createForagingAnt, args = (a,b) )
-    antTwoThread.start()
-    antTwoThread.join()
+
     
-    antThrThread = threading.Thread( target = createForagingAnt, args = (a,b) )
-    antThrThread.start()
-    antThrThread.join()
- 
-    antForThread = threading.Thread( target = createForagingAnt, args = (a,b) )
-    antForThread.start()
-    antForThread.join()
-
-
-
 
 
 
